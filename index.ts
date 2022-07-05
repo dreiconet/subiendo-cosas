86247b090cb764d0542af907b0f93a93a1b1004e
dreiconet <dreiconet@gmail.com>
2022-07-05 18:51:46 -0300

    primer commit

---
+++ index.ts
@@ -0,0 +1,9 @@
+import { sumarDosNumeros} from "./otro-modulo";
+
+
+function main(){
+    console.log(sumarDosNumeros(3,5));
+
+}
+
+main ();
---
+++ otro-modulo.ts
@@ -0,0 +1,11 @@
+//primero los imports
+
+//segundo codigo que son objetos, funciones, variables
+
+// al final los exports
+function sumarDosNumeros(num1:number, num2:number){
+    return num1 + num2;
+
+}
+
+export {sumarDosNumeros};
